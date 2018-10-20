03bdf7fde43e0065511e447b1d435d7d6654ffc4
Ashley Coleman <me@ashleycoleman.me>
2018-10-20 11:44:54 -0500

    Week 9 Lab Starting files

---
+++ core.h
@@ -0,0 +1,24 @@
+//
+//  core.h
+//  Project 3 
+//
+//  Created by Ashley Coleman on 7/5/18.
+//  Copyright © 2018 Ashley Coleman. All rights reserved.
+//
+
+#ifndef core_h
+#define core_h
+
+#include <stdio.h>
+
+#define ALLOC(type, count) NULL 
+
+#define SET_ARRAY(array, value, length) { }
+
+#define SWAP(T, a, b) { }
+
+#define SQUARE(a) a 
+
+int core_main(int argc, const char * argv[]);
+
+#endif /* core_h */
